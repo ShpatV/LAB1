718f91d76eab99edf5977c591d4dc4ddb07bc3d4
shpatvata1 <shpatvata1@gmail.com>
2022-05-17 01:56:29 +0200

    restored

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,14 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt=>
+            {
+                opt.AddPolicy("CorsPolicy",policy=>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                    
+                });
+            });
 
         }
 
@@ -53,6 +61,8 @@ namespace API
            // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
